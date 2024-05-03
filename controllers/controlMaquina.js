216d07ae2061d884e0e3b7f1e019beecb9c7537a
DuverneyGarcia <duver.gc33@gmail.com>
2024-05-03 00:44:13 -0500

    Creando mi primer proyecto en python

---
+++ controllers/controlMaquina.js
@@ -0,0 +1,7 @@
+// typing text animation script
+var typed = new Typed(".typing", {
+    strings: ["Energía solar", "Sostenibilidad energética", "Innovación ambiental"],
+    typeSpeed: 40,
+    backSpeed: 20,
+    loop: true
+});
